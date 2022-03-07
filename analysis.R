950fa50acfb1a32fedce95bdb43bf24ea00d3655
Matt Condrin <mattcondrin@Matts-MacBook-Pro.local>
2022-03-07 14:34:23 -0800

    done

--- analysis.R
+++ analysis.R
@@ -1,6 +1,42 @@
 # Workbook 6: analyze NHANES data
 
 # Set up
-library(foreign)
 library(survey)
 library(Hmisc)
+
+demo <- sasxport.get('DEMO_I.XPT')
+alco <- sasxport.get('ALQ_I.XPT')
+
+nhanes <- merge(x = demo, y = alco, by = 'seqn', all = TRUE)
+
+wt_sum <- sum(nhanes$wtint2yr, na.rm = TRUE)
+
+#Analysis
+nhanes$alq151[nhanes$alq151 == 2] <- 0
+nhanes$alq151[nhanes$alq151 == 7] <- NA
+nhanes$alq151[nhanes$alq151 == 9] <- NA
+
+
+
+nhanes_survey <- svydesign(
+  id = ~sdmvpsu,
+  nest = TRUE,
+  strata = ~sdmvstra,
+  weights = ~wtint2yr,
+  data = nhanes
+)
+
+
+nhanes_mean <- svymean(nhanes_survey, mean)
+
+mean_by_gender <- svyby(~, ~riagender, dclus1, svymean)
+
+
+
+
+
+
+
+
+
+
